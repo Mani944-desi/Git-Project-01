1534ef5d9c033d9be8863448ccf1cf37eb7a8542
Ubuntu <ubuntu@ip-172-31-21-255.us-west-2.compute.internal>
2025-11-29 03:58:10 +0000

    Commit after new stash proof demo

--- script1.sh
+++ script1.sh
@@ -2,4 +2,8 @@
 echo "This is the third time I am doing for the script-1"
 echo "Feature: Now Added logging to script1"
 echo "Change from main branch"
+<<<<<<< Updated upstream
 echo "stash test"
+=======
+new stash proof
+>>>>>>> Stashed changes
